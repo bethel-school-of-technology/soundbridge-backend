83283fee5561e04901a66ded3acf404697bfd853
Anthony Martinez <Martinezanthony213@gmail.com>
2019-09-10 11:23:44 -0700

    nodemon
    added nodemon to dev dependancies to allow for smoother development

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function (req, res) {
+  res.send('You successfully created a POST route!');
+});
+
+router.put('/', function (req, res) {
+  res.send('You successfully created a PUT route!');
+});
+
+router.delete('/', function (req, res) {
+  res.send('You successfully created a DELETE route!');
+});
 module.exports = router;
