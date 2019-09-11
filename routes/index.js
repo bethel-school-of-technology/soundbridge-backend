b5a23bfc24dadda6281304d30c135da9c2415e3a
Anthony Pereira <anthonyp@ichop.org>
2019-09-10 17:19:17 -0700

    Successfully logging into Spotify and redirecting to frontend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var SPOTIFY_CLIENT_ID = "94f0fc9ce18b4809bf951ec27dee0021";
+var redirect_uri = 'http://localhost:3000/logged-in';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', function (req, res) {
+  var scopes = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize' +
+    '?response_type=code' +
+    '&client_id=' + SPOTIFY_CLIENT_ID +
+    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + 
+    '&redirect_uri=' + redirect_uri );
+});
+
+router.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        process.env.SPOTIFY_CLIENT_ID
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = 'http://localhost:3000/logged-in'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+})
 
 module.exports = router;
