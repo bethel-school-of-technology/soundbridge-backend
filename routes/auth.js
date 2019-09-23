3e8906ebd1e934c629105aaf76b57961911e7f3c
Anthony Martinez <Martinezanthony213@gmail.com>
2019-09-23 16:28:54 -0700

    Log out now functional
    Jwt token now is set to null. Logout button now fully functional.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var Router = express.Router();
 var User = require('../models/User');
-var {registerValidation, loginValidation} = require('../validation');
+var { registerValidation, loginValidation } = require('../validation');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
@@ -71,55 +71,71 @@ Router.route('/delete/:id').get(function (req, res) {
 
 Router.post('/register', async (req, res, next) => {
 
-    //Data Validation for Users
-    const { error } = registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
+  //Data Validation for Users
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
 
-    //Checking if email already exists
-    const emailExist = await User.findOne({email: req.body.email});
-    if( emailExist ) return res.status(400).send('Email already Exists');
+  //Checking if email already exists
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already Exists');
 
-    //Password Hashing
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  //Password Hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
 
-    //Create a New User
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-    });
-    try {
-        const savedUser = await user.save();
-        res.send({user: user._id});
-    } catch (err) {
-        res.status(400).send(err);
-    }
+  //Create a New User
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  });
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 
 //Login
 
-Router.post('/login',  async (req,res) => {
-     //Data Validation for Users
-     const { error } = loginValidation(req.body);
-     if(error) return res.status(400).send(error.details[0].message);
+Router.post('/login', async (req, res) => {
+  //Data Validation for Users
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-     //Checking if email exists
-    const user = await User.findOne({email: req.body.email});
-    if( !user ) return res.status(400).send('Email is not Found');
+  //Checking if email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Email is not Found');
 
-    //Checking if Password is correct
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if(!validPass) return res.status(400).send("Invalid Password");
-
-    //Create and Assign a JWT
-    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
-    res.header('auth-Token', token).send(token);
+  //Checking if Password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid Password");
  
+  if (user) {
+    //Create and Assign a JWT
+    const token = jwt.sign(
+      { _id: user._id },
+      process.env.TOKEN_SECRET,
+      { expiresIn: '1h' });
+    res.cookie('jwt', token);
+    res.send('Login Successful');
+  } else {
+    console.log('Wrong Password');
+    res.redirect('login')
+  }
+
 })
 
+
+//Logout
+
+Router.get('/logout', function(req,res){
+  res.cookie('jwt', "", {expires: new Date(0)});
+  res.send('Logged Out')
+})
 
 module.exports = Router;
